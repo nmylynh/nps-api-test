6e5884fb2d1e399f3884a5716dbffa19b68b3f57
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-23 07:05:53 -0800

    Create users-model.js

---
+++ api/models/users-model.js
@@ -0,0 +1,67 @@
+const db = require('../../data/dbConfig');
+
+module.exports = {
+    update,
+    remove,
+    getUserRoles,
+    get,
+    getUsers
+}
+
+
+function getUsers() {
+    let users = db('users');
+
+    let newUsersArray = users.map(user => {
+
+        return this.get(user.id)
+    })
+
+    return newUsersArray
+}
+
+function get(id) {
+    let users = db('users');
+  
+    if (id) {
+      users.where({ id }).first();
+  
+      const promises = [users, this.getUserRoles(id)]; 
+  
+      return Promise.all(promises).then(results => {
+        let [user, roles] = results;
+  
+        if (user) {
+            user.roles = getOnlyRoles(roles);
+        
+            return user
+          } else {
+            return null;
+          }
+        });
+    }
+  
+    return users
+}
+
+function getUserRoles(id) {
+    let loadRoles = db('user_roles')
+        .where('user_id', id)
+        .join('roles', 'user_roles.role_id', 'roles.id')
+        .join('users', 'user_roles.user_id', 'users.id')
+        .select('user_roles.id', 'users.id as user_id' , 'users.username', 'roles.role')
+
+    return loadRoles
+}
+
+function update(id, changes) {
+    return db('users')
+    .where({ id })
+    .update(changes, '*')
+}
+
+function remove(id) {
+    return db('users')
+    .where({ id })
+    .del();
+}

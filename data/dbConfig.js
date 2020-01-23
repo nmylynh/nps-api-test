742d3345533d6258f4f16bcd3b53e77f02d6f0f7
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-23 07:06:02 -0800

    Create dbConfig.js

---
+++ data/dbConfig.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const knex = require('knex');
+const config = require('../knexfile.js');
+
+const environment = process.env.DB_ENV || 'development';
+
+console.log('environment', environment)
+
+module.exports = knex(config[environment]);

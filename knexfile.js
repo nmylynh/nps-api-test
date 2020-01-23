4df3d472f27be9771961bab801b50c2b8a5af2e3
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-23 07:06:14 -0800

    Create knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const localPG = db => ({
+  host: process.env.HOST,
+  database: db,
+  user: process.env.USER,
+  password: process.env.PASS || ''
+});
+
+const pgTest = localPG(process.env.DB_TEST);
+const pgDev = localPG(process.env.DB_DEV);
+
+const dbSettings = (connection) => ({
+  client: 'pg',
+  connection,
+  pool: {
+    min: 2,
+    max: 10
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: './data/migrations'
+  },
+  seeds: {
+    directory: `./data/seeds`
+  }
+});
+
+module.exports = {
+  testing: dbSettings(pgTest),
+  development: dbSettings(pgDev),
+  production: dbSettings(process.env.DATABASE_URL),
+  onUpdateTrigger: table => `
+  CREATE TRIGGER ${table}_updated_at
+  BEFORE UPDATE ON ${table}
+  FOR EACH ROW
+  EXECUTE PROCEDURE on_update_timestamp();
+`
+};

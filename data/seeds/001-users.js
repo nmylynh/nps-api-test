57d61fd446778e8cf729fd592aaaa1a355c57a8d
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-25 20:45:28 -0800

    Update 001-users.js

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -2,17 +2,14 @@ const bcrypt = require("bcryptjs");
 exports.seed = function(knex, Promise) {
   const users = [
     {
-      id: 1,
       username: "thisispatrick",
       password: bcrypt.hashSync('test', 10)
     },
     {
-      id: 2,
       username: "patrick",
       password: bcrypt.hashSync('test', 10)
     },
     {
-      id: 3,
       username: "mitsy",
       password: bcrypt.hashSync('test', 10)
     }

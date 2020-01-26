205f2b42b2ba7b777c968585ecc36b44a97ceab2
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-25 18:45:09 -0800

    Update 001-users.js

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,13 +1,29 @@
+const bcrypt = require("bcryptjs");
+exports.seed = function(knex, Promise) {
+  const users = [
+    {
+      id: 1,
+      username: "thisispatrick",
+      password: bcrypt.hashSync('test', 10)
+    },
+    {
+      id: 2,
+      username: "patrick",
+      password: bcrypt.hashSync('test', 10)
+    },
+    {
+      id: 3,
+      username: "mitsy",
+      password: bcrypt.hashSync('test', 10)
+    }
+  ];
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return (
+    knex
+      // Deletes ALL existing entries for users table
+      .raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+      .then(function() {
+        return knex("users").insert(users);
+      })
+  );
 };

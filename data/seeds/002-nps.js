130c9d6cc4861a63f668632d67aa77353606e26e
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-25 18:45:12 -0800

    Create 002-nps.js

---
+++ data/seeds/002-nps.js
@@ -0,0 +1,45 @@
+exports.seed = function(knex, Promise) {
+  const nps = [
+    {
+      id: 1,
+      user_id: 3,
+      name: "Daydreamers",
+      description: "Daydreamers is an app that adds a screen saver and a song to your phone when you're in daydreaming mode",
+      total_promoters: 3,
+      total_passives: 2,
+      total_detractors: 2,
+      nps_score: 14
+
+    },
+    {
+      id: 1,
+      user_id: 3,
+      name: "Daydreamers1",
+      description: "Daydreamers is an app that adds a screen saver and a song to your phone when you're in daydreaming mode1",
+      total_promoters: 3,
+      total_passives: 2,
+      total_detractors: 2,
+      nps_score: 14
+
+    },
+    {
+      id: 1,
+      user_id: 3,
+      name: "Daydreamers3",
+      description: "Daydreamers is an app that adds a screen saver and a song to your phone when you're in daydreaming mode3",
+      total_promoters: 3,
+      total_passives: 2,
+      total_detractors: 2,
+      nps_score: 14
+    }
+  ];
+
+  return (
+    knex
+      // Deletes ALL existing entries for nps table
+      .raw("TRUNCATE TABLE nps RESTART IDENTITY CASCADE")
+      .then(function() {
+        return knex("nps").insert(nps);
+      })
+  );
+};

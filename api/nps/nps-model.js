fea22d23aafadfb83142e95e565e0da869d78d73
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-01-25 18:29:23 -0800

    Create nps-model.js

---
+++ api/nps/nps-model.js
@@ -0,0 +1,35 @@
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return db('nps');
+}
+
+function findById(id) {
+    return db('nps')
+    .where({ id })
+    .first()
+}
+
+async function add(nps) {
+    const [id] = await db('nps').insert(nps);
+
+    return findById(id);
+}
+
+function update(id, changes) {
+    return db('nps')
+    .where({ id })
+    .update(changes, '*')
+}
+
+function remove(id) {
+    return db('nps')
+    .where({ id })
+    .del();
+}
